342c8eda95a4dbc6efd3d98c64cdfac6bb8b1d6a
adasheaisvam.com <adash.eai@svam.com>
2023-03-08 20:48:55 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Home from './screens/Home';
+import Live from './screens/Live';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  const options = {headerShown: false};
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={options} />
+        <Stack.Screen name="Live" component={Live} options={options} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/Live.js
@@ -0,0 +1,226 @@
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  PermissionsAndroid,
+  ActivityIndicator,
+  Dimensions,
+  Share,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+
+import RtcEngine, {
+  ChannelProfile,
+  ClientRole,
+  RtcLocalView,
+  RtcRemoteView,
+  VideoRemoteState,
+} from 'react-native-agora';
+
+const dimensions = {
+  width: Dimensions.get('window').width,
+  height: Dimensions.get('window').height,
+};
+
+const videoStateMessage = state => {
+  switch (state) {
+    case VideoRemoteState.Stopped:
+      return 'Video turned off by Host';
+
+    case VideoRemoteState.Frozen:
+      return 'Connection Issue, Please Wait';
+
+    case VideoRemoteState.Failed:
+      return 'Network Error';
+  }
+};
+
+async function requestCameraAndAudioPermission() {
+  try {
+    const granted = await PermissionsAndroid.requestMultiple([
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+    ]);
+    if (
+      granted['android.permission.RECORD_AUDIO'] ===
+        PermissionsAndroid.RESULTS.GRANTED &&
+      granted['android.permission.CAMERA'] ===
+        PermissionsAndroid.RESULTS.GRANTED
+    ) {
+      console.log('You can use the cameras & mic');
+    } else {
+      console.log('Permission denied');
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+export default function Live(props) {
+  const isBroadcaster = props.route.params.type === 'create';
+
+  const onShare = async () => {
+    try {
+      const result = await Share.share({message: props.route.params.channel});
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const [joined, setJoined] = useState(false);
+  const [broadcasterVideoState, setBroadcasterVideoState] = useState(
+    VideoRemoteState,
+  );
+  const AgoraEngine = useRef();
+  const init = async () => {
+    AgoraEngine.current = await RtcEngine.create(
+      'c7e742d5df23478285a9dc4f4ff62407',
+    );
+    AgoraEngine.current.enableVideo();
+    AgoraEngine.current.setChannelProfile(ChannelProfile.LiveBroadcasting);
+    if (isBroadcaster) {
+      AgoraEngine.current.setClientRole(ClientRole.Broadcaster);
+    }
+
+    AgoraEngine.current.addListener('RemoteVideoStateChanged', (uid, state) => {
+      if (uid === 1) {
+        setBroadcasterVideoState(state);
+      }
+    });
+
+    AgoraEngine.current.addListener(
+      'JoinChannelSuccess',
+      (channel, uid, elapsed) => {
+        console.log('JoinChannelSuccess', channel, uid, elapsed);
+        setJoined(true);
+      },
+    );
+  };
+
+  const onSwitchCamera = () => AgoraEngine.current.switchCamera();
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      requestCameraAndAudioPermission();
+    }
+    const uid = isBroadcaster ? 1 : 0;
+    init().then(() =>
+      AgoraEngine.current.joinChannel(
+        null,
+        props.route.params.channel,
+        null,
+        uid,
+      ),
+    );
+    return () => {
+      AgoraEngine.current.destroy();
+    };
+  }, []);
+
+  const renderHost = () =>
+    broadcasterVideoState === VideoRemoteState ? (
+      <RtcRemoteView.SurfaceView
+        uid={1}
+        style={styles.fullscreen}
+        channelId={props.route.params.channel}
+      />
+    ) : (
+      <View style={styles.broadcasterVideoStateMessage}>
+        <Text style={styles.broadcasterVideoStateMessageText}>
+          {videoStateMessage(broadcasterVideoState)}
+        </Text>
+      </View>
+    );
+
+  const renderLocal = () => (
+    <RtcLocalView.SurfaceView
+      style={styles.fullscreen}
+      channelId={props.route.params.channel}
+    />
+  );
+
+  return (
+    <View style={styles.container}>
+      {!joined ? (
+        <>
+          <ActivityIndicator
+            size={60}
+            color="#222"
+            style={styles.activityIndicator}
+          />
+          <Text style={styles.loadingText}>Joining Stream, Please Wait</Text>
+        </>
+      ) : (
+        <>
+          {isBroadcaster ? renderLocal() : renderHost()}
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={onShare}>
+              <Text style={styles.buttonText}>Share</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={onSwitchCamera}>
+              <Text style={styles.buttonText}>Switch Camera</Text>
+            </TouchableOpacity>
+          </View>
+        </>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loadingText: {
+    fontSize: 18,
+    color: '#222',
+  },
+  fullscreen: {
+    width: dimensions.width,
+    height: dimensions.height,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 0,
+  },
+  button: {
+    width: 150,
+    backgroundColor: '#fff',
+    marginBottom: 50,
+    paddingVertical: 13,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonText: {
+    fontSize: 17,
+  },
+  broadcasterVideoStateMessage: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#222',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  broadcasterVideoStateMessageText: {
+    color: '#fff',
+    fontSize: 20,
+  },
+});
